199a40c261dde1777c35a011aeaf9c3cc582c158
agyeievans <evanskofiagyei06@gmail.com>
2023-03-31 16:00:24 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">
+
+          </p>
+          <p class="date">
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    height: 100vh;
+    width: 100vw;
+    background-color: white;
+  }
+
+  .container{
+    max-width: 1000px;
+    padding: 10px;
+    margin: 0 auto;
+  }
+
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  h1{
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 75px;
+    color: black;
+  }
+
+  button{
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21, 20, 20);
+    border-radius: 100%;
+    color:white;
+    font-size: 20px;
+  }
+</style>
